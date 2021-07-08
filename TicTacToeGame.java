4232be7f9fe52e94fb99aae05cbf00a192dfc47b
yashpurwar1 <yashpurwar.yashpurwar.com>
2021-07-08 09:30:54 +0530

    Added a board

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -2,8 +2,17 @@ package ticTacToe;
 
 public class TicTacToeGame {
 	
+	public void GameBoard() {
+		char board[] = new char[10];
+		for (int index=1; index<=9; index++) {
+			board[index]=' ';
+		}
+		
+	}
 	public static void main(String args[]) {
 		System.out.println("Welcome to tic tac toe");
+		TicTacToeGame obj = new TicTacToeGame();
+		obj.GameBoard();
 	}
 
 }
